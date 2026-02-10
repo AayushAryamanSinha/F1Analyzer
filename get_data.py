71017d9a21e2db4308f492c39ad722253da682a9
Aayush Sinha <41622590+goldenboy123868@users.noreply.github.com>
2026-02-10 15:08:30 -0800

    added comments

--- get_data.py
+++ get_data.py
@@ -2,19 +2,22 @@ import kagglehub
 import os
 import shutil
 
-#Ge
+#Downloading datasets to the path
 src_path = kagglehub.dataset_download(
     "rohanrao/formula-1-world-championship-1950-2020"
 )
 
+#Defining path 
 DST = os.path.join("datasets", "raw")
 
 
+#Checking if it exists or needs to be made
 os.makedirs(DST, exist_ok=True)
 
 
 count = 0
 
+#Copying from src to DST 
 for filename in os.listdir(src_path):
     if filename.endswith(".csv"):
         shutil.copy(
@@ -23,6 +26,7 @@ for filename in os.listdir(src_path):
         )
         count += 1
 
-
+#Confirmation
 print(f"Copied {count} files")
+
 
